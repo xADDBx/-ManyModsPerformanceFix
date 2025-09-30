eafc5d69f16ae9c010af5d6dfb496d3324a9465d
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-09-30 15:03:52 +0200

    1.3.0; added a transpiler that rmeoves a call to CheatsManager.CollectAll at the beginning of the CheatsManager constructor. The result of this call is ignored and it has (as far as I can see) no side effects. CollectAll is later called again in GetInternals where the result is actually used.

--- !ManyModsPerformanceFix/Main.cs
+++ !ManyModsPerformanceFix/Main.cs
@@ -1,10 +1,10 @@
-﻿using HarmonyLib;
-using UnityModManagerNet;
-using Core.Cheats;
+﻿using Core.Cheats;
+using HarmonyLib;
 using Kingmaker.Visual.Particles;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Reflection.Emit;
+using UnityEngine;
+using UnityModManagerNet;
 
 namespace ManyModsPerformanceFix;
 
@@ -14,9 +14,31 @@ public static class Main {
     public static bool Load(UnityModManager.ModEntry modEntry) {
         Log = modEntry.Logger;
         HarmonyInstance = new(modEntry.Info.Id);
+        Remove_NoOp_From_CheatsManagerInit();
         Apply_ManyModsLoad_PerformanceFix();
         Apply_SnapMapBase_UpdateRuntimeData_PerformanceFix();
         return true;
+    }
+    private static void Remove_NoOp_From_CheatsManagerInit() {
+        var target = AccessTools.Constructor(typeof(CheatsManager), []);
+        var patch = AccessTools.Method(typeof(Main), nameof(Main.CheatsManagerInit_Remvoe_NoOp_Transpiler));
+        HarmonyInstance.Patch(target, transpiler: new(patch));
+    }
+    private static IEnumerable<CodeInstruction> CheatsManagerInit_Remvoe_NoOp_Transpiler(IEnumerable<CodeInstruction> instructions) {
+        var m = AccessTools.Method(typeof(CheatsManager), nameof(CheatsManager.CollectAll));
+        bool skip = false;
+        foreach (var inst in instructions) {
+            if (skip) {
+                skip = false;
+                continue;
+            }
+            if (inst.Calls(m)) {
+                skip = true;
+                continue;
+            } else {
+                yield return inst;
+            }
+        }
     }
     private static void Apply_ManyModsLoad_PerformanceFix() {
         _ = CheatsManagerHolder.Instance;
